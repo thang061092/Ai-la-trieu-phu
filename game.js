c4a9351a69ce416a78640e9e1b6cc546e20b2055
thang061092 <63143765+thang061092@users.noreply.github.com>
2020-05-04 08:43:25 +0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,102 @@
+let time = 0;
+let fullTime = 20;
+let widthTime = 500;
+
+class Game {
+    constructor(number) {
+        this.quizs = [];
+        this.count = 0;
+        this.number = number;
+        this.point = 0;
+
+    }
+    getCorrects() {
+        return this.quizs.correct;
+    }
+    selectAnswer(info) {
+        return this.quizs.answer(info) == this.getCorrects() ? true : false;
+    }
+    creatQuizs() {
+        for (let i = 0; i < this.number; i++) {
+            let quiz = new Quiz(questions[i][0], questions[i][1], questions[i][2], questions[i][3]);
+            this.quizs.push(quiz);
+        }
+    }
+    showQuizs() {
+        document.getElementById('point').innerHTML = "Point : " + this.quizs[this.count].score + " Điểm";
+        document.getElementById('question-number').innerHTML = "Question " + (this.count + 1) + " :";
+        document.getElementById('question').innerHTML = this.quizs[this.count].question;
+        for (let i = 0; i < this.quizs[this.count].answer.length; i++) {
+            document.getElementById('' + i).innerHTML = this.quizs[this.count].answer[i];
+        }
+        time = 0;
+    }
+    selectAnswers(info) {
+        let select = this.quizs[this.count].selectAnswer(info);
+        if (!select) {
+            document.getElementById("false").play();
+            this.checkPoint();
+            this.count = 0;
+        } else {
+            this.count++;
+            document.getElementById("true").play();
+            console.log(this.count);
+        }
+        if (this.count >= this.number) {
+            alert("Xin chúc mừng bạn là triệu phú");
+            this.count = 0;
+        }
+        this.showQuizs();
+    }
+    checkPoint() {
+        if (this.count < 5) {
+            this.point = 0;
+        } else if (this.count >= 5 && this.count < 10) {
+            this.point = 4;
+        } else if (this.count >= 10 && this.count < 15) {
+            this.point = 9;
+        }
+        alert("Bạn ra về với số tiền thưởng là :" + this.quizs[this.point].score + "000 vnđ");
+    }
+    exitss() {
+        this.showQuizs();
+        this.count += 1;
+    }
+    gameOver() {
+        console.log("gameOver")
+        document.getElementById("main").style.display = 'none';
+        document.getElementById("start-game").style.display = "block";
+        location.reload();
+    }
+}
+function countDown() {
+    let timeDiv = document.getElementById("time");
+    let run = setInterval(function () {
+        time += 0.1;
+        timeDiv.style.width = widthTime * time / fullTime + "px";
+        if (time >= fullTime) {
+            document.getElementById("lose").play();
+            game.checkPoint();
+            clearInterval(run);
+            game.gameOver();
+        }
+    }, 100);
+
+}
+let game = null;
+function start() {
+    document.getElementById('main').style.display = "block";
+    document.getElementById('start-game').style.display = 'none';
+    document.getElementById('sound').play();
+    game = new Game(15);
+    game.creatQuizs();
+    console.log(game);
+    game.showQuizs();
+    countDown();
+}
+function stop() {
+    alert("Bạn thua cuộc và không nhận được gì ^^");
+    document.getElementById("main").style.display = 'none';
+    document.getElementById("start-game").style.display = "block";
+    document.getElementById("lose").play();
+}
---
+++ quiz.js
@@ -0,0 +1,91 @@
+const questions = [
+    ['Nhất quỷ nhì ..., Thứ ba học ...?', ["ma, trò", "tam, sinh", "trò, ma", "trời, đất"], "ma, trò", 200],
+    ['Ai cũng biết đỉnh núi Everest cao nhất thế giới, vậy trước khi đỉnh Everest được khám phá, đỉnh núi nào cao nhất thế giới?', ["Himalaya", "Everest", "Phan Xi Păng", "Phú Sĩ"], "Everest", 400],
+    ["Đâu là một loại hình chợ tạm tự phát thường xuất hiện trong các khu dân cư?", ["Ếch", "Nhái", "Thằn lằn", "Cóc",], "Cóc", 600],
+    ["Đâu là tên một loại bánh Huế?", ["khoái", "sướng", "vui", "thích",], "khoái", 1000],
+    ["Nhà văn Kim Dung (Trung Quốc) nổi tiếng với thể loại văn học gì?", ["Truyện trinh thám", "Tiểu thuyết kiếm hiệp", "Sử thi", "Thơ lãng mạn",], "Tiểu thuyết kiếm hiệp",2000],
+    ["Bộ phim (Chị Dậu) được chuyển thể từ tác phẩm nào?", ["Người mẹ cầm súng", "Vợ chồng A Phủ", "Tắt đèn", "Tuổi thơ dữ dội",], "Tắt đèn", 3000],
+    ["Đại Ngu là quốc hiệu của triều đại nào?", ["Triều Ngô", "Triều Hồ", "Các chúa Nguyễn", "Nhà Tây Sơn",], "Triều Hồ", 6000],
+    ["Phi hành gia Phạm Tuân đã mang loại thực vật nào để nghiên cứu trên con tàu vũ trụ Phương Đông?", ["Bèo tấm", "Bèo tổ ong", "Bèo hoa dâu", "Bèo cái",], "Bèo hoa dâu", 10000],
+    ["Đâu là loại cháo khác với các món còn lại", ["cháo gà", "cháo heo", "cháo vịt", "cháo bò",], "cháo heo", 14000],
+    ["Vũ điệu truyền thống của đảo Hawaii là ?", ["Điệu nhảy Hula", "Điệu nhảy cha cha cha", "Vũ điệu cồng chiêng", "Vũ điệu SamBa",], "Điệu nhảy Hula", 22000],
+    ["Ngôn ngữ nào được nhiều người sử dụng chính thức nhất ?", ["Tiếng Anh", " Tiếng Đức", "Tiếng Trung quốc", "Tiếng Pháp",], "Tiếng Trung quốc", 30000],
+    ["Ở người có bao nhiêu cặp nghiễm sắc thể ?", ["23 cặp", "22 cặp", "13 cặp", "12 cặp",], "23 cặp", 40000],
+    ["Trên tay của Lý Công Uẩn khi sinh ra đã có 4 chữ son đó là những chữ nào ?", ["Sơn Hà Xã Tắc", "Giang Sơn Xã Tắc", "Xã Tắc Giang Sơn", "Xã Tắc Sơn Hà",], "Sơn Hà Xã Tắc", 60000],
+    ["Tỉnh nào sau đây không có một ngọn núi nào ?", [" Thái Bình", " Nam Định", "Quảng Ninh", "Quảng Nam",], " Thái Bình", 85000],
+    ["hệ số thập phân là phát minh của đất nước nào ?", ["Nga", "Nhật Bản", "Ấn Độ", "Trung Quốc",], "Ấn Độ", 150000],
+];
+
+class Quiz {
+    constructor(question, answer, correct, score) {
+        this.question = question;
+        this.answer = answer;
+        this.correct = correct;
+        this.score = score;
+    }
+
+    getCorrect() {
+        return this.correct;
+    }
+
+    selectAnswer(index) {
+        return this.answer[index] == this.getCorrect() ? true : false;
+    }
+}
+
+let quizs = [];
+let count = 0;
+
+function creatQuiz() {
+
+    for (let i = 0; i < questions.length; i++) {
+        let quiz = new Quiz(questions[i][0], questions[i][1], questions[i][2], questions[i][3]);
+        quizs.push(quiz);
+    }
+}
+
+function showQuiz() {
+    document.getElementById('point').innerHTML = "Point : " + quizs[count].score + "$";
+    document.getElementById('question-number').innerHTML = "Question " + (count + 1) + " :";
+    document.getElementById('question').innerHTML = quizs[count].question;
+    for (let i = 0; i < quizs[count].answer.length; i++) {
+        document.getElementById('' + i).innerHTML = quizs[count].answer[i];
+    }
+
+
+}
+
+function selectAnswer(index) {
+    let select = quizs[count].selectAnswer(index);
+    // count = select ? count += 1 : 0;
+    if (!select) {
+        checkPoint();
+        count = 0;
+    } else {
+        count += 1;
+         console.log(count);
+    }
+    if (count >= questions.length) {
+        alert("Bạn Là triệu phú");
+        count = 0;
+    }
+    showQuiz();
+}
+
+function checkPoint() {
+    let point = 0;
+    if (count < 5) {
+        point = 0;
+    } else if (count >= 5 && count < 10) {
+        point = 5;
+    } else if (count >= 10 && count < 15) {
+        point = 10;
+    }
+    alert("Bạn ra về với số tiền thưởng là :" + quizs[point].score);
+
+}
+// creatQuiz();
+// showQuiz();
+// console.log(quizs);
+//
+
